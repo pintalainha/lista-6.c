f3e622a0595a01c9a129ac1023b6de18dd50b516
pintalainha <vr5409822@gmail.com>
2026-05-13 19:24:46 -0300

    Create exc1.
    Fix syntax errors and improve input prompts.

---
+++ exc1.c
@@ -0,0 +1,16 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   int numeros[10];
+   int pos
+   for (pos = 0; pos < 10; pos++){
+       printf("Digite o numero da posição %d: ", pos);
+       scanf("%D", &numero[pos]);
+    for (pos = 0 <10; pos ++){
+        printf("digite a numero da posição %s: ", pos);
+        
+    }
+   }
+    return 0;
+}
